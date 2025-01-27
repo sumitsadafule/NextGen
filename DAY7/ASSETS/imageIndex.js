9f2998de98df247177ec620c84a38f2933d30841
sumeet suhas sadafule <sumitsadafule0303@gmail.com>
2025-01-27 22:56:31 +0530

    Add files via upload

---
+++ DAY7/ASSETS/imageIndex.js
@@ -0,0 +1,11 @@
+import normalWeight from './normal.jpeg'
+import obese from './obese.png'
+import overWeight from './overweight.png'
+import underWeight from './underweight.jpeg'
+
+export const assets = { 
+    normalWeight, 
+    obese, 
+    overWeight, 
+    underWeight
+}
