53b4a922a47aaef44df8d5ab1d1c2758a1c11620
sumeet suhas sadafule <sumitsadafule0303@gmail.com>
2025-01-27 23:17:43 +0530

    Add files via upload

---
+++ DAY11/CLIENT/SRC/MODEL/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: String,
+        required: true
+    }, 
+    phone: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
