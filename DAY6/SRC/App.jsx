a112f2b4023b38fdae4439f8f2ef7bfc02e5aaa1
sumeet suhas sadafule <sumitsadafule0303@gmail.com>
2025-01-27 22:43:19 +0530

    Add files via upload

---
+++ DAY6/SRC/App.jsx
@@ -0,0 +1,18 @@
+import Carousal from './component/Carousal'
+import Counter from './component/Counter'
+import Demo from './component/Demo'
+import Navbar from './component/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <Carousal/>
+    <Demo/>
+    <Counter/>
+    </>
+  )
+}
+
+export default App
